36dfa25212a3a287839aeb75a6830a6601915a07
digwall <digwall@hotmail.com>
2021-05-22 23:28:17 +0800

    refactor(runtime):  refine

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -21,9 +21,12 @@
 pub mod currency {
     use parami_primitives::Balance;
 
-    pub const MILLICENTS: Balance = 1_000_000_000;
-    pub const CENTS: Balance = 1_000 * MILLICENTS; // assume this is worth about a cent.
-    pub const DOLLARS: Balance = 100 * CENTS;
+    // Default for node-template: 15
+    // Other chains: 12, 18
+    pub const UNITS: Balance = 1_000_000_000_000_000;
+    pub const DOLLARS: Balance = UNITS;
+    pub const CENTS: Balance = DOLLARS / 100;
+    pub const MILLICENTS: Balance = CENTS / 1_000;
 
     pub const fn deposit(items: u32, bytes: u32) -> Balance {
         items as Balance * 15 * CENTS + (bytes as Balance) * 6 * CENTS
