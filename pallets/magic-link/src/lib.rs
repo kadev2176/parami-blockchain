cbd96edd5efc3a6e147d4bb0af65f9f8b3145373
chenwei <chenwei767@gmail.com>
2021-08-19 20:31:46 +0800

    Create magic account.

--- pallets/magic-link/src/lib.rs
+++ pallets/magic-link/src/lib.rs
@@ -10,30 +10,34 @@ use frame_support::{
 use frame_system::pallet_prelude::*;
 pub use parami_primitives::Balance;
 use sp_runtime::{
-	traits::{AccountIdConversion, One, Saturating, Verify},
-	DispatchErrorWithPostInfo, FixedPointNumber, PerU16,
+	traits::{AccountIdConversion, One, Saturating},
+	DispatchErrorWithPostInfo,
 };
-use sp_std::vec::Vec;
+
+macro_rules! s {
+	($e: expr) => {
+		sp_runtime::SaturatedConversion::saturated_into($e)
+	};
+}
 
 mod mock;
 mod tests;
 
-pub type BalanceOf<T> =
-	<<T as pallet::Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
-pub const UNIT: Balance = 1_000_000_000_000_000;
+mod types;
+pub use types::*;
 
 pub use self::pallet::*;
 #[frame_support::pallet]
 pub mod pallet {
 	use super::*;
 
-	#[pallet::pallet]
+    #[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(PhantomData<T>);
 
 	#[pallet::config]
 	#[pallet::disable_frame_system_supertrait_check]
-	pub trait Config: pallet_timestamp::Config<AccountId = parami_primitives::AccountId> {
+	pub trait Config: pallet_timestamp::Config {
 		/// The overarching event type.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
 
@@ -48,12 +52,13 @@ pub mod pallet {
 	#[pallet::metadata(T::AccountId = "AccountId")]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		CreatedAdvertiser(T::AccountId, u8, u8),
+        /// StableAccount changed \[stash, controller, magic\]
+        StableAccount(T::AccountId, T::AccountId, T::AccountId),
 	}
 
 	#[pallet::error]
 	pub enum Error<T> {
-		SomethingTerribleHappened,
+        NoAvailableId,
 	}
 
 	#[pallet::hooks]
@@ -81,6 +86,18 @@ pub mod pallet {
 		fn build(&self) {}
 	}
 
+	/// Next available ID.
+	#[pallet::storage]
+	pub type NextId<T: Config> = StorageValue<_, GlobalId, ValueQuery>;
+
+    /// map from controller account to `StableAccount`
+    #[pallet::storage]
+    pub type StableAccounts<T: Config> = StorageMap<_, Twox64Concat, T::AccountId, StableAccountOf<T>>;
+
+    /// map from magic account to controller account
+    #[pallet::storage]
+    pub type StableAccountByMagic<T: Config> = StorageMap<_, Twox64Concat, T::AccountId, T::AccountId>;
+
 	/// an index for rewards. The secondary key: `(user_did, media_did)`
 	#[pallet::storage]
 	pub type Rewards<T: Config> =
@@ -90,14 +107,50 @@ pub mod pallet {
 	impl<T: Config> Pallet<T> {
 		#[pallet::weight(1_000_000_000)]
 		#[transactional]
-		pub fn create_advertiser(
+		pub fn create_stable_account(
 			origin: OriginFor<T>,
-			#[pallet::compact] reward_pool: Balance,
+			magic_account: T::AccountId,
+			#[pallet::compact] stash_deposit: Balance,
 		) -> DispatchResultWithPostInfo {
-			let who: T::AccountId = ensure_signed(origin)?;
+            let controller_account = ensure_signed(origin)?;
+			let sa: StableAccountOf<T> = StableAccount {
+				created_time: now::<T>(),
+				stash_account: Self::create_stash_account(Self::inc_id()?),
+                controller_account,
+				magic_account,
+			};
+
+            <T as Config>::Currency::transfer(&sa.controller_account, &sa.magic_account, s!(FEE), KeepAlive)?;
+            <T as Config>::Currency::transfer(&sa.controller_account, &sa.stash_account, s!(stash_deposit), KeepAlive)?;
+
+            Self::update_stable_account(&sa);
 			Ok(().into())
 		}
 	}
 }
 
-impl<T: Config> Pallet<T> {}
+/// now is duration since unix epoch in millisecond
+pub fn now<T: Config>() -> T::Moment {
+	pallet_timestamp::Pallet::<T>::now()
+}
+
+impl<T: Config> Pallet<T> {
+    fn update_stable_account(sa: &StableAccountOf<T>) {
+        StableAccounts::<T>::insert(&sa.controller_account, sa);
+        StableAccountByMagic::<T>::insert(&sa.magic_account, &sa.controller_account);
+        Self::deposit_event(Event::StableAccount(sa.stash_account.clone(), sa.controller_account.clone(), sa.magic_account.clone()));
+    }
+
+	fn create_stash_account(id: GlobalId) -> T::AccountId {
+		let stab_acc = PalletId(*b"prm/stab");
+		stab_acc.into_sub_account(id)
+	}
+
+	fn inc_id() -> Result<GlobalId, DispatchError> {
+		NextId::<T>::try_mutate(|id| -> Result<GlobalId, DispatchError> {
+			let current_id = *id;
+			*id = id.checked_add(GlobalId::one()).ok_or(Error::<T>::NoAvailableId)?;
+			Ok(current_id)
+		})
+	}
+}
--- pallets/magic-link/src/tests.rs
+++ pallets/magic-link/src/tests.rs
@@ -3,7 +3,6 @@
 use super::{Event as AdEvent, *};
 use crate::mock::{Event as MEvent, *};
 use frame_support::{assert_noop, assert_ok};
-use sp_core::Pair;
 
 #[test]
 fn create_advertiser_should_work() {
