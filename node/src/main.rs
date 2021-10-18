9a434b098c7286a89ba2653a1881fdc65ab283f1
OctopusR <Right.Sun@outlook.com>
2021-10-18 17:42:23 +0800

    fix stackoverflow by increase stack size

--- node/src/main.rs
+++ node/src/main.rs
@@ -7,7 +7,12 @@ mod service;
 mod cli;
 mod command;
 mod rpc;
-
+use std::thread;
 fn main() -> sc_cli::Result<()> {
-    command::run()
+
+    let child = thread::Builder::new().stack_size(32 * 1024 * 1024).spawn(move ||-> sc_cli::Result<()> {
+        command::run()
+    }).unwrap();
+
+    child.join().unwrap()
 }
