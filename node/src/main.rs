3e702bab9453328297bf008d592173b7b54c1d31
7IN0SAN9 <me@7in0.me>
2021-11-08 01:14:52 +0800

    fix stackoverflow on windows

--- node/src/main.rs
+++ node/src/main.rs
@@ -8,6 +8,17 @@ mod cli;
 mod command;
 mod rpc;
 
+#[cfg(not(target_os = "windows"))]
 fn main() -> sc_cli::Result<()> {
     command::run()
+}
+
+#[cfg(target_os = "windows")]
+fn main() -> sc_cli::Result<()> {
+    let child = std::thread::Builder::new()
+        .stack_size(32 * 1024 * 1024)
+        .spawn(move || -> sc_cli::Result<()> { command::run() })
+        .unwrap();
+
+    child.join().unwrap()
 }
