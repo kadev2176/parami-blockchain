86f886ac8daa1ca06d3f12c1c2f328338e16415e
7IN0SAN9 <me@7in0.me>
2022-01-06 19:02:14 +0800

    add e2e tests

---
+++ .maintain/e2e.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Start the node and save the pid
+
+nohup ./target/release/parami --alice --chain dev --tmp --unsafe-ws-external --rpc-cors all --unsafe-rpc-external --rpc-methods Unsafe --enable-offchain-indexing true > /dev/null 2>&1 &
+
+pid=$!
+
+# Wait for the node to start
+while ! lsof -i:9944; do
+  sleep 0.1
+done
+
+# Run the tests
+
+cd ./e2e/
+npm i
+npm test
+
+# Stop the node
+
+kill -9 $pid
