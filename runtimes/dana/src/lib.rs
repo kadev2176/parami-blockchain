547f6ab3d297d2ad7ede9da78cccd3d0e95d4a65
Ruibin Xing <xingrobin@gmail.com>
2022-06-02 15:21:24 +0800

    mod: bump version 327 -> 328 (#58)

--- runtimes/dana/src/lib.rs
+++ runtimes/dana/src/lib.rs
@@ -171,7 +171,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("parami"),
     impl_name: create_runtime_str!("parami-node"),
     authoring_version: 20,
-    spec_version: 327,
+    spec_version: 328,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 2,
