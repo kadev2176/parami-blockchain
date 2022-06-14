f660c2e80248406c8e03694220cd515dddd1eb92
Chino Chang <me@7in0.me>
2022-06-14 15:03:35 +0800

    fix e2e tests (#65)

--- utils/e2e.ts
+++ utils/e2e.ts
@@ -15,20 +15,10 @@ import { submit } from './utils';
   const alice = keyring.addFromUri('//Alice');
 
   const m = keyring.addFromUri(await mnemonicGenerate(12));
-  const c = keyring.addFromUri(await mnemonicGenerate(12));
 
   spinnies.add('root', {
-    text: ` Root Account: ${m.address}`,
+    text: ` User Account: ${m.address}`,
     status: 'succeed',
-  });
-
-  spinnies.add('codo', {
-    text: ` Codo Account: ${c.address}`,
-    status: 'succeed',
-  });
-
-  spinnies.add('cash', {
-    text: ' Cash Account: pending...',
   });
 
   spinnies.add('did', {
@@ -37,6 +27,10 @@ import { submit } from './utils';
 
   spinnies.add('kol', {
     text: '          KOL: pending...',
+  });
+
+  spinnies.add('nft', {
+    text: '          NFT: pending...',
   });
 
   spinnies.add('advertiser', {
@@ -50,116 +44,78 @@ import { submit } from './utils';
   // 1. New User
 
   spinnies.add('preparing', {
-    text: 'Airdropping...',
-  });
-  await submit(
-    chain,
-    chain.tx.balances.transfer(c.address, 3_000n * 10n ** 18n),
-    alice
-  );
-  spinnies.remove('preparing');
-
-  await submit(
-    chain,
-    chain.tx.magic.createStableAccount(m.address, 2_000n * 10n ** 18n),
-    c
-  );
-  const stableOf = await chain.query.magic.metadata(c.address);
-  const s = (stableOf.toHuman() as any).stashAccount;
-  spinnies.succeed('cash', { text: ` Cash Account: ${s}` });
-
-  spinnies.add('preparing', {
     text: 'Registering DID...',
   });
-  await submit(chain, chain.tx.magic.codo(chain.tx.did.register(null)), c);
+  await submit(
+    chain,
+    chain.tx.balances.transfer(m.address, 3_000n * 10n ** 18n),
+    alice
+  );
+  await submit(chain, chain.tx.did.register(null), m);
   spinnies.remove('preparing');
-
-  const didOf = await chain.query.did.didOf(s);
+  const didOf = await chain.query.did.didOf(m.address);
   const did = didOf.toString();
   spinnies.succeed('did', { text: `          DID: ${did}` });
 
   // 2. Prepare KOL
 
-  const km = keyring.addFromUri(await mnemonicGenerate(12));
-  const kc = keyring.addFromUri(await mnemonicGenerate(12));
+  const k = keyring.addFromUri(await mnemonicGenerate(12));
 
   spinnies.add('preparing', {
     text: 'Creating KOL...',
   });
-
   await submit(
     chain,
-    chain.tx.balances.transfer(kc.address, 3_000n * 10n ** 18n),
+    chain.tx.balances.transfer(k.address, 3_000n * 10n ** 18n),
     alice
   );
-
-  await submit(
-    chain,
-    chain.tx.magic.createAccountsAndDid(km.address, 2_000n * 10n ** 18n, null),
-    kc
-  );
-
-  const ks = await chain.query.magic.metadata(kc.address);
-  const kolOf = await chain.query.did.didOf((ks.toHuman() as any).stashAccount);
+  await submit(chain, chain.tx.did.register(null), k);
+  const kolOf = await chain.query.did.didOf(k.address);
   const kol = kolOf.toString();
   spinnies.update('kol', { text: `          KOL: ${kol}` });
 
   spinnies.update('preparing', {
+    text: 'Creating NFT...',
+  });
+  await submit(chain, chain.tx.nft.kick(), k);
+  const nftOf = await chain.query.nft.preferred(kol);
+  const nft = nftOf.toString();
+  spinnies.update('nft', { text: `          NFT: ${nft}` });
+
+  spinnies.update('preparing', {
     text: 'Backing KOL...',
   });
-  await submit(
-    chain,
-    chain.tx.magic.codo(chain.tx.nft.back(kol, 1_000n * 10n ** 18n)),
-    c
-  );
+  await submit(chain, chain.tx.nft.back(nft, 1_000n * 10n ** 18n), m);
 
   spinnies.update('preparing', {
     text: 'Minting...',
   });
-  await submit(
-    chain,
-    chain.tx.magic.codo(chain.tx.nft.mint('Test Token', 'XTT')),
-    kc
-  );
+  await submit(chain, chain.tx.nft.mint(nft, 'Test Token', 'XTT'), k);
   spinnies.remove('preparing');
+  spinnies.succeed('nft');
   spinnies.succeed('kol');
 
   // 3. Prepare Advertiser
 
-  const am = keyring.addFromUri(await mnemonicGenerate(12));
-  const ac = keyring.addFromUri(await mnemonicGenerate(12));
+  const a = keyring.addFromUri(await mnemonicGenerate(12));
 
   spinnies.add('preparing', {
     text: 'Creating Advertiser...',
   });
-
   await submit(
     chain,
-    chain.tx.balances.transfer(ac.address, 3_000n * 10n ** 18n),
+    chain.tx.balances.transfer(a.address, 3_000n * 10n ** 18n),
     alice
   );
-
-  await submit(
-    chain,
-    chain.tx.magic.createAccountsAndDid(am.address, 2_000n * 10n ** 18n, null),
-    ac
-  );
-
-  const as = await chain.query.magic.metadata(ac.address);
-  const aderOf = await chain.query.did.didOf(
-    (as.toHuman() as any).stashAccount
-  );
+  await submit(chain, chain.tx.did.register(null), a);
+  const aderOf = await chain.query.did.didOf(a.address);
   const ader = aderOf.toString();
   spinnies.update('advertiser', { text: `   Advertiser: ${ader}` });
 
   spinnies.update('preparing', {
     text: 'Depositing to become Advertiser...',
   });
-  await submit(
-    chain,
-    chain.tx.magic.codo(chain.tx.advertiser.deposit(1_000n * 10n ** 18n)),
-    ac
-  );
+  await submit(chain, chain.tx.advertiser.deposit(1_000n * 10n ** 18n), a);
   spinnies.remove('preparing');
   spinnies.succeed('advertiser');
 
@@ -170,36 +126,33 @@ import { submit } from './utils';
   spinnies.add('preparing', {
     text: 'Creating Tags...',
   });
-  await submit(chain, chain.tx.magic.codo(chain.tx.tag.create(tag)), ac);
+  await submit(chain, chain.tx.tag.create(tag), a);
 
   spinnies.update('preparing', {
     text: 'Creating Advertisement...',
   });
   await submit(
     chain,
-    chain.tx.magic.codo(
-      chain.tx.ad.create(
-        2n * 10n ** 18n,
-        [tag],
-        'ipfs://QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG',
-        10,
-        500000
-      )
+    chain.tx.ad.create(
+      2n * 10n ** 18n,
+      [tag],
+      'ipfs://QmYwAPJzv5CZsnA625s3Xf2nemtYgPpHdWEz79ojWnPbdG',
+      10,
+      500000,
+      1n * 10n ** 18n,
+      1n * 10n ** 18n,
+      50n * 10n ** 18n
     ),
-    ac
+    a
   );
-  const adsOf = await chain.query.ad.adsOf.entries();
-  const ad = (adsOf[0][1].toHuman() as any)[0];
+  const adsOf = await chain.query.ad.adsOf(ader);
+  const ad = (adsOf.toHuman() as any)[0];
   spinnies.update('ad', { text: `Advertisement: ${ad}` });
 
   spinnies.update('preparing', {
     text: 'Bidding...',
   });
-  await submit(
-    chain,
-    chain.tx.magic.codo(chain.tx.ad.bid(ad, kol, 1n * 10n ** 18n)),
-    ac
-  );
+  await submit(chain, chain.tx.ad.bid(ad, nft, 1n * 10n ** 18n, null, null), a);
   spinnies.succeed('ad');
 
   spinnies.remove('preparing');
@@ -207,14 +160,10 @@ import { submit } from './utils';
   // 4. Payout
 
   spinnies.add('pay', { text: 'Paying...' });
-  const before = await chain.query.assets.account(0, s);
+  const before = await chain.query.assets.account(0, m.address);
   const beforeBalance = (before as any).balance.toHuman().replaceAll(',', '');
-  await submit(
-    chain,
-    chain.tx.magic.codo(chain.tx.ad.pay(ad, kol, did, [[tag, 5]], null)),
-    ac
-  );
-  const after = await chain.query.assets.account(0, s);
+  await submit(chain, chain.tx.ad.pay(ad, nft, did, [[tag, 5]], null), a);
+  const after = await chain.query.assets.account(0, m.address);
   const afterBalance = (after as any).balance.toHuman().replaceAll(',', '');
   spinnies.succeed('pay', { text: `Paid ${afterBalance - beforeBalance}` });
 
