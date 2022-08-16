b332c0c1c6bd52bdb40bf1f643983f8b2d37e19a
kadev2176 <kevinchristopher2176@gmail.com>
2022-08-16 12:35:31 +0800

    MOD: patch for comment

--- pallets/nft/src/benchmarking.rs
+++ pallets/nft/src/benchmarking.rs
@@ -36,7 +36,6 @@ benchmarks! {
     }: _(RawOrigin::Signed(caller.clone()))
     verify {
         let did = Did::<T>::did_of(&caller).unwrap();
-        // assert_eq!(<Preferred<T>>::get(&did), Some(Zero::zero()));
     }
 
     back {
